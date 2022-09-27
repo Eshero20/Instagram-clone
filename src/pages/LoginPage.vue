b2b98c56a6b413ce1ab17125a7628c2e29972e4b
Eshero20 <82354914+Eshero20@users.noreply.github.com>
2022-09-27 12:50:02 -0400

    changes to layout

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,4 +1,4 @@
-<template>
+<!--<template>
   <q-page class="flex flex-center">
     <img
       alt="Instagram logo"
@@ -24,3 +24,80 @@ export default defineComponent({
   name: "IndexPage",
 });
 </script>
+-->
+<template>
+  <q-page
+    class="bg-white window-height window-width row justify-center items-center"
+  >
+    <div>
+      <img
+        src="~/assets/instagram-front-page-phone.png"
+        style="width: 220px; height: 330px"
+      />
+    </div>
+    <div class="column">
+      <div class="row">
+        <h5 class="text-h5 text-black q-my-md"></h5>
+      </div>
+      <div class="row">
+        <q-card square bordered class="q-pa-lg shadow-1">
+          <q-card-section>
+            <q-form class="q-gutter-md">
+              <div>
+                <img src="~assets/instagram-front-logo.png" style="width: 200px;
+                height: 50px" object fit: cover />
+              </div>
+              <q-input
+                square
+                filled
+                clearable
+                v-model="email"
+                type="email"
+                label="Phone number, username, or email"
+              />
+              <q-input
+                square
+                filled
+                clearable
+                v-model="password"
+                type="password"
+                label="Password"
+              />
+            </q-form>
+          </q-card-section>
+          <q-card-actions class="q-px-md">
+            <q-btn
+              unelevated
+              color="light-blue"
+              size="lg"
+              class="full-width"
+              label="Login"
+            />
+          </q-card-actions>
+          <q-card-section class="text-center q-pa-none">
+            <p class="text-grey-6">Don't have an account? Sign up</p>
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "LoginPage",
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+};
+</script>
+
+<style>
+.q-card {
+  width: 360px;
+}
+</style>
+
