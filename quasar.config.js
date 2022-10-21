18f465a566ee06c0ecb102a92274a53a5108598c
Anthony Bergen <anthonydbergen@gmail.com>
2022-10-21 10:49:13 -0400

    resolved merge conflicts

--- quasar.config.js
+++ quasar.config.js
@@ -43,20 +43,20 @@ module.exports = configure(function (/* ctx */) {
     extras: [
       // 'ionicons-v4',
       // 'mdi-v5',
-      // 'fontawesome-v6',
+      'fontawesome-v6',
       // 'eva-icons',
       // 'themify',
       // 'line-awesome',
       // 'roboto-font-latin-ext', // this or either 'roboto-font', NEVER both!
 
       'roboto-font', // optional, you are not bound to it
-      'material-icons', // optional, you are not bound to it
+      // 'material-icons', // optional, you are not bound to it
     ],
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#build
     build: {
       target: {
-        browser: [ 'es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1' ],
+        browser: ['es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1'],
         node: 'node16'
       },
 
@@ -93,6 +93,7 @@ module.exports = configure(function (/* ctx */) {
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#devServer
     devServer: {
       // https: true
+      port: 8080,
       open: true // opens browser window automatically
     },
 
@@ -133,7 +134,7 @@ module.exports = configure(function (/* ctx */) {
     // https://v2.quasar.dev/quasar-cli/developing-ssr/configuring-ssr
     ssr: {
       // ssrPwaHtmlFilename: 'offline.html', // do NOT use index.html as name!
-                                          // will mess up SSR
+      // will mess up SSR
 
       // extendSSRWebserverConf (esbuildConf) {},
       // extendPackageJson (json) {},
@@ -144,7 +145,7 @@ module.exports = configure(function (/* ctx */) {
       // manualPostHydrationTrigger: true,
 
       prodPort: 3000, // The default port that the production server should use
-                      // (gets superseded if process.env.PORT is specified at runtime)
+      // (gets superseded if process.env.PORT is specified at runtime)
 
       middlewares: [
         'render' // keep this as last one
--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,11 +1,13 @@
 <script setup>
 import { useQuasar } from "quasar";
 import { ref } from "vue";
+import { useRouter } from "vue-router";
 
 const $q = useQuasar();
+const router = useRouter();
 
-const name = ref(null);
-const age = ref(null);
+const username = ref(null);
+const password = ref(null);
 const accept = ref(false);
 
 const onSubmit = () => {
@@ -27,169 +29,113 @@ const onSubmit = () => {
 };
 
 const onReset = () => {
-  name.value = null;
-  age.value = null;
+  username.value = null;
+  password.value = null;
   accept.value = false;
 };
 </script>
 
 <template>
-  <q-page class="window-height window-width row justify-center items-center">
-    <div class="row">
-      <div class="col-5 q-py-xl">
-        <q-img height="500px" width="300px" src="../assets/phones.png" />
+  <q-page
+    class="window-height window-width row justify-center items-center q-px-xl"
+  >
+    <div class="row q-px-xl">
+      <div class="col-6 text-right">
+        <q-img height="650px" width="450px" src="../assets/phones.png" />
       </div>
-      <div class="col-6 q-pr-sm" style="width: 55%">
-        <q-page class="q-pa-md">
-          <q-card
-            square
-            flat
-            bordered
-            class="shadow-10; text-center; q-pa-xl"
-            style="width:360px;height:460px; fit;"
-          >
-            <div class="loginbox">
-              <q-img
-                width="250px"
-                class="q-pr-xl"
-                position="5px 0px "
-                src="../assets/instagram-logo.png"
-              />
-              <q-form
-                @submit="onSubmit"
-                @reset="onReset"
-                class="row"
-                style="max-width: 250px"
-              >
-                <q-input
-                  outlined
-                  dense
-                  v-model="name"
-                  class="col-12"
-                  label="Phone number, username, or email"
-                  lazy-rules
-                  :rules="[
-                    (val) => (val && val.length > 0) || 'Please type log in',
-                  ]"
-                />
+      <div class="col-6 q-px-xl text-left">
+        <q-card square flat bordered class="text-center">
+          <q-card-section>
+            <q-img width="250px" src="../assets/instagram-logo.png" />
+          </q-card-section>
+          <q-form @submit="onSubmit" @reset="onReset" class="row q-px-xl">
+            <q-input
+              outlined
+              dense
+              v-model="username"
+              class="col-12"
+              label="Phone number, username, or email"
+              lazy-rules
+              :rules="[
+                (val) => (val && val.length > 0) || 'Please type log in',
+              ]"
+            />
 
-                <q-input
-                  outlined
-                  dense
-                  type="password"
-                  v-model="password"
-                  class="col-12"
-                  label="Password"
-                  lazy-rules
-                  :rules="[
-                    (val) =>
-                      (val !== null && val !== '') ||
-                      'Please type your password',
-                    (val) =>
-                      (val > 0 && val < 100) || 'Please type your password',
-                  ]"
-                />
+            <q-input
+              outlined
+              dense
+              type="password"
+              v-model="password"
+              class="col-12"
+              label="Password"
+              lazy-rules
+              :rules="[
+                (val) =>
+                  (val !== null && val !== '') || 'Please type your password',
+                (val) => (val > 0 && val < 100) || 'Please type your password',
+              ]"
+            />
 
-                <q-btn
-                  class="col-12"
-                  label="Log In"
-                  type="submit"
-                  color="primary"
-                />
-              </q-form>
-              <div class="q-pa-md">
-                <q-separator color="black" inset />
-              </div>
-            </div>
-            <div class="text-center">
-              <q-btn flat src="src/assets/facebook-icon.png">
-                <q-avatar square size="16px">
-                  <img
-                    src="src/assets/facebook-icon.png"
-                    label="Log in with Facebook"
-                    style=""
-                  />
-                </q-avatar>
-                <div class="text-lowercase">log in with Facebook</div>
-              </q-btn>
-            </div>
-
-            <!-- Forgot Instagram Password -->
-            <a href="#"></a>
-            <div class="text-center">
-              <q-btn
+            <q-btn
+              class="col-12"
+              label="Log In"
+              type="submit"
+              color="primary"
+            />
+          </q-form>
+          <div class="q-pa-md">
+            <q-separator color="black" inset />
+          </div>
+          <div class="text-center">
+            <q-btn
+              label="log in with Facebook"
+              flat
+              icon="fa-brands fa-square-facebook"
+              color="blue-10"
+            />
+            <br /><q-btn
+              flat
+              class="text-body2 text-weight-light text-capitalize"
+              label="Forgot password?"
+              color="blue-10"
+            />
+          </div>
+        </q-card>
+        <div class="col-6 q-pt-md">
+          <q-card square flat bordered class="text-center q-pa-md">
+            <div>
+              Don't have an account?<q-btn
                 flat
-                class="q-pl"
-                label="Forgot password?"
-                color="blue-2"
+                color="primary"
+                label="Sign up"
+                class="text-capitalize q-px-xs"
               />
             </div>
           </q-card>
-          <div class="col-6 q-pr-ms" style="width: 55%">
-            <q-card
-              square
-              flat
-              bordered
-              class="shadow-10; text-center; q-pa-lg"
-              style="width:360px;height:100px; fit;"
-            >
-              <div class="q-pa-md q-gutter-sm text-center" style="width: 100%">
-                <div class="col-gutter-sm">
-                  <div class="justify-center">
-                    <div class="col-12">
-                      <div>
-                        Don't have an account?<q-btn
-                          flat
-                          color="primary"
-                          label="Sign up"
-                          class="text-capitalize text-no-wrap"
-                        />
-                      </div>
-                    </div>
-                    <div></div>
-                  </div>
-                </div>
-              </div>
-            </q-card>
-            <div class="text-right">Get App.</div>
-            <q-page class="q-xy-md">
 
-                <div class="row justify-center">
-                  <div class="col-12 col-md">
-                    <a
-                      href="https://apps.apple.com/us/app/instagram/id389801252?itsct=apps_box_badge&amp;itscg=30200"
-                      ><img
-                        alt="Download on the App Store"
-                        src="src/assets/download on the App Store.png"
-                        width="150"
+          <div class="text-center q-pa-lg">Get the app.</div>
 
-                    /></a>
-                  </div>
-                  <div class="col-12 col-md-2">
-                    <a
-                      href="ttps://play.google.com/store/apps/details?id=com.instagram.android&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"
-                      ><img
-                        src="src/assets/Get it on Google Play.png"
-                        alt="Get it on Google Play"
-                        width="150"
-                    /></a>
-                  </div>
-
-                </div>
-
-            </q-page>
+          <div class="row justify-center">
+            <div class="col-6 text-right q-pr-sm">
+              <img
+                @click="router.push('/')"
+                alt="Download on the App Store"
+                src="src/assets/download on the App Store.png"
+                width="150"
+              />
+            </div>
+            <div class="col-6 text-left q-pl-sm">
+              <img
+                src="src/assets/Get it on Google Play.png"
+                alt="Get it on Google Play"
+                width="150"
+              />
+            </div>
           </div>
-        </q-page>
+        </div>
       </div>
     </div>
   </q-page>
 </template>
 
-<style>
-.buttons {
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  margin: 10px 0px 700px 100px;
-}
-</style>
+<style></style>
