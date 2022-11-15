06cc75fe880d4c5c7a40e968b540a16fec679240
Eric <eric@Erics-MacBook-Air.local>
2022-11-15 14:08:38 -0500

    main layout updates

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-layout view="lHh lpR ffr">
+  <q-layout view="lHh LpR Ffr">
     <q-avatar>
       <q-img width="500px" src="../assets/instagram-logo.png" />
     </q-avatar>
@@ -9,18 +9,19 @@
       </q-avatar>
     </q-drawer>
 
-    <q-drawer show-if-above bordered v-model="rightDrawerOpen" side="right">
+    <q-drawer show-if-above  v-model="rightDrawerOpen" side="right">
       <div class="q-pa-lg">
             <RouterLink class="active" to="/">
               <q-avatar size="75px">
                 <img src="https://cdn.quasar.dev/img/avatar.png">
-                
+
               </q-avatar>
             </RouterLink>
       </div>
     </q-drawer>
 
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered clas="absolute-fixed">
+     <q-scroll-area class="fit">
       <q-list>
         <div class="q-pa-lg">
           <q-img src="src/assets/instagram-front-logo.png" style="width: 100px">
@@ -36,7 +37,7 @@
             />
           </div>
         </div>
-        <q-footer>
+        <q-footer class="absolute-fixed">
           <q-btn
             flat
             color="black"
@@ -80,16 +81,14 @@
           </q-btn>
         </q-footer>
       </q-list>
+    </q-scroll-area>
     </q-drawer>
+  <q-page-container>
+        <q-page>
+      <q-card square flat bordered class="text-right" style="width: auto">
+          <div class="row q-pa-md justify-center">
+            <div class="q-pr-xs q-gutter-xs col-12" style="max-width: 550px; height: 70px">
 
-<q-page-container>
-<q-page bordered padding>
-  <div class="col-5 q-pa-md">
-    <q-card square flat bordered class="text-center" style="width: auto">
-      <div class="col q-pr-xs text-left">
-        <div class="row q-pa-md justify-between items-center">
-          <div bordered class="col-20 self-center offset-3 overflow:auto; style=circle border">
-            <div class="q-pr-xs q-gutter-md">
             <RouterLink class="active" to="/">
               <q-avatar size="75px">
                 <img src="https://cdn.quasar.dev/img/avatar.png">
@@ -117,28 +116,44 @@
             </RouterLink>
             </div>
           </div>
-        </div>
-      </div>
+
+
+      </q-card>
+    <q-seperator />
+    <div class="q-pa-md items-start q-gutter-md">
+    <q-card
+      v-for="post in posts"
+      :key="post.id"
+      class="my-card" flat bordered>
+      <q-item>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="src/assets/InstaProfilePic.png">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label class="text-bold">James Madison</q-item-label>
+          <q-item-label caption>
+            Rocky Mountains, United States
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator />
+    <div class="q-pa-md row">
+      <q-img src="https://cdn.quasar.dev/img/mountains.jpg" />
+
+
+        <q-card-section>
+        <div>Rocky Mounts</div>
+        <div class="text-caption text-grey">November 10 9:05am</div>
+        </q-card-section>
+    </div>
     </q-card>
   </div>
-    <q-seperator />
-    <div class="col-13 q-pa-md" style="width: auto">
-      <q-card square flat bordered class="text-center col-5 q-pa-sm">
-      <div class="q-pa-xs justify-between items-center">
-        <div bordered class="col-20 self-center offset-3 overflow:auto; style=circle border">
-          <div class="q-pr-xs q-gutter-md">
-            <img src="https://cdn.quasar.dev/img/mountains.jpg" style="width: 400px" />
-              <q-card-actions align="right">
-                <q-btn flat round color="red" icon="favorite" />
-                <q-btn flat round color="teal" icon="bookmark" />
-                <q-btn flat round color="primary" icon="share" />
-              </q-card-actions>
-          </div>
-        </div>
-      </div>
-      </q-card>
-   </div>
-</q-page>
+  </q-page>
+
             <router-view />
     </q-page-container>
   </q-layout>
@@ -209,6 +224,36 @@ export default defineComponent({
       toggleLeftDrawer() {
         leftDrawerOpen.value = !leftDrawerOpen.value;
       },
+      posts:[{
+          id: 1,
+          caption: 'Rocky Mounts',
+          date: 1591776655504,
+          location: 'Rocky Mountains, United States',
+          ImageUrl: 'https://cdn.quasar.dev/img/mountains.jpg'
+      },
+      {
+          id: 2,
+          caption: 'Rocky Mounts',
+          date: 1591776655504,
+          location: 'Rocky Mountains, United States',
+          ImageUrl: 'https://cdn.quasar.dev/img/mountains.jpg'
+      },
+      {
+          id: 3,
+          caption: 'Rocky Mounts',
+          date: 1591776655504,
+          location: 'Rocky Mountains, United States',
+          ImageUrl: 'https://cdn.quasar.dev/img/mountains.jpg'
+      },
+      {
+          id: 4,
+          caption: 'Rocky Mounts',
+          date: 1591776655504,
+          location: 'Rocky Mountains, United States',
+          ImageUrl: 'https://cdn.quasar.dev/img/mountains.jpg'
+      },
+
+    ]
     };
   },
 });
